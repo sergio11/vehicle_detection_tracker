f61ae189c61a682739351f4d42f65219c39e23e4
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-07 21:40:43 +0100

    Update OpenCV example

--- examples/OpenCvTrackerTest.py
+++ examples/OpenCvTrackerTest.py
@@ -1,9 +1,6 @@
 from VehicleDetectionTracker.VehicleDetectionTracker import VehicleDetectionTracker
 
-# Approximate values for traffic cameras
-focal_length_mm = 8.0  # Focal length in millimeters
-sensor_size_mm = 1/3  # Sensor size in inches
-video_path = "https://49-d2.divas.cloud/CHAN-8293/CHAN-8293_1.stream/playlist.m3u8?77.211.5.136&vdswztokenhash=mzG-kruh8fv5WKEjXYCT0Tlbc23hSuoDRXidQ-aE0zk="
+video_path = "[[YOUR_STREAMING_SOURCE]]"
 vehicle_detection = VehicleDetectionTracker()
 result_callback = lambda result: print({
     "number_of_vehicles_detected": result["number_of_vehicles_detected"],
@@ -14,9 +11,9 @@ result_callback = lambda result: print({
             "detection_confidence": vehicle["detection_confidence"],
             "color_info": vehicle["color_info"],
             "model_info": vehicle["model_info"],
-            "speed_kph": vehicle["speed_kph"]
+            "speed_info": vehicle["speed_info"]
         }
         for vehicle in result['detected_vehicles']
     ]
 })
-vehicle_detection.process_video(video_path, result_callback = result_callback, focal_length_mm=focal_length_mm, sensor_size_mm=sensor_size_mm)
+vehicle_detection.process_video(video_path, result_callback = result_callback)
